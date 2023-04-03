1836d58b96703a0f05bf4b3e40ecf5836f30b180
stavonofa <afadganteng1414@gmail.com>
2023-04-03 11:49:26 +0700

    LAB 4 WEB

---
+++ lab4_php_modular/footer.php
@@ -0,0 +1,6 @@
+<footer>
+  <p>&copy; 2023, Teknik Informatika, <a href="https://wa.me/6285591529798" style="text-decoration: none;">irfan fadlurrahman</a>, Universitas Pelita Bangsa</p>
+  </div>
+  </body>
+
+  </html>
